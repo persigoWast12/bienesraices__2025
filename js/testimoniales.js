8f366830917b8528c78b787be4804472f189a67b
Persigo Martín <persigomartin@gmail.com>
2025-06-23 23:40:11 -0300

    Subida n1

---
+++ js/testimoniales.js
@@ -0,0 +1,40 @@
+const USERS_TESTIMONIALES=[
+    {
+        "name":"Julio Garro",
+        "opinion":"Excelente asesoramiento durante todo el proceso de compra. Me ayudaron a encontrar la casa perfecta en Belgrano dentro de mi presupuesto. Muy profesionales y siempre disponibles para resolver dudas.",
+        "ranked":5
+    },
+    {
+        "name":"Carlos Mendoza",
+        "opinion":"Muy buen servicio de alquiler, me consiguieron inquilinos confiables rápidamente. Solo me hubiera gustado más comunicación durante las visitas.",
+        "ranked":4
+    },
+    {
+        "name": "Ana Lopéz",
+        "opinion":"Poca atención al cliente, mucha tardanza para la resolucion de mi consulta.",
+        "ranked":2.5
+    }
+
+]
+
+function renderTestimonios(){
+    const container=document.getElementById("testimoniales-container");
+    container.innerHTML="";
+
+    USERS_TESTIMONIALES.forEach((testi,index) =>{
+        const testimonialesDiv=document.createElement("div");
+        testimonialesDiv.classList.add("testimonial");
+    
+        testimonialesDiv.innerHTML=`
+            <img src="./img/comillas.png" alt="comillas" class="img-testimoniales">
+            <p>${testi.opinion}</p>
+            <p>${testi.ranked}/5</p>
+            <cite>- ${testi.name}</cite>
+    
+        `;
+        container.appendChild(testimonialesDiv);
+    });
+}
+
+
+renderTestimonios()
